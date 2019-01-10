6eebeb2b7c53b29f06c37366dbd4c7387cd9a475
sabinav23 <sabinav23@gmail.com>
2019-01-10 21:04:47 +0200

    Update mock students

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,8 @@ public class Main {
     public static void main(String[] args) throws NameFieldEmptyException, GenderException, YearOfBirthException, IDEmptyOrStudentNotFoundException {
 
         StudentRepository repository = new StudentRepository();
-        repository.addStudent("Gemi", "Zemi", "1999-01-01", "m", 56135445);
-        repository.addStudent("EU", "Tu", "1999-01-02", "f", 51256);
-
-
+        repository.addStudent("John", "Doe", "1999-01-01", "m", 56135445);
+        repository.addStudent("Student", "One", "1999-01-02", "f", 51256);
 
         System.out.println(repository.getStudentsByAge(20));
         System.out.println(repository.getStudentsSorted());
